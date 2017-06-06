52eb5bf9aeb6081f986890fb8f08b88b8d1b7c8a
Xu Waycell <xw901103@gmail.com>
2017-06-06 19:38:06 +1000

    Remove `targets`, `dependencies ` and `exclude`
    - Remove `targets`, `dependencies ` and `exclude` from Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,5 @@
 import PackageDescription
 
 let package = Package(
-	name: "Hashids",
-	targets: [],
-	dependencies: urls.map { .Package(url: $0, majorVersion: 0) },
-	exclude: []
+	name: "Hashids"
 )
